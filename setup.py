2ddd90d6c28c8cd5080f701778ae305d5430d79c
kreczko <lkreczko@googlemail.com>
2016-08-16 11:51:34 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,54 @@
-#!/usr/bin/env python
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='hepshell',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.1',
     description='A Python Shell for High Energy Particle Physics',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/kreczko/hepshell',
+
     author='Luke Kreczko',
     author_email='lkreczko@gmail.com',
-    packages=['hepshell', 'hepshell.commands'],
+
+    license='Apache 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Interpreters',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    package_data={
+        'examples': ['cli-ex', 'env-minimal.sh', 'env-full.sh'],
+    },
 )
