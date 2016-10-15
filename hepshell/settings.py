fed41cf86bc07690d14e70c27e953d867935ae60
kreczko <lkreczko@googlemail.com>
2016-10-15 21:32:08 +0100

    added hepshell.settings

---
+++ hepshell/settings.py
@@ -0,0 +1,21 @@
+import os
+
+'''
+    List of modules which should be loaded as commands for hepshell
+'''
+COMMANDS = [
+    'hepshell.commands'
+]
+
+# load ENV variables as settings if available
+ENV_PREFIX = 'HEPSHELL_'
+for key in os.environ:
+    if key.startswith(ENV_PREFIX):
+        name = key[len(ENV_PREFIX):].upper()
+        globals()[name] = os.environ[key]
+
+# if hepshell_settings exists in PYTHONPATH import all definitions
+try:
+    from hepshell_settings import *
+except ImportError:
+    pass
