c4814cdf55c2957416896157bceef45a0d2706ed
kreczko <lkreczko@googlemail.com>
2017-04-22 22:12:27 +0100

    draft for logging module

---
+++ hepshell/logger.py
@@ -0,0 +1,22 @@
+from hepshell import HEP_PROJECT_ROOT
+import hepshell.SETTINGS as s
+
+logger = logging.getLogger(s.LOG['name'])
+logger.setLevel(logging.INFO)
+if s.LOG['logToFile']:
+    formatter = logging.Formatter(
+    '%(asctime)s [%(name)s]  %(levelname)s: %(message)s')
+
+    logfile = s.LOG['logFile']
+    fh = logging.FileHandler(logfile)
+    fh.setFormatter(formatter)
+    fh.setLevel(s.LOG['logLevelFile'])
+    logger.addHandler(fh)
+
+if s.LOG['logToConsole']:
+    # logging to the console
+    formatter = logging.Formatter('%(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(s.LOG['logLevelConsole'])
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
